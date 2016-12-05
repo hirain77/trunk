5fbd9acaedcbeff11fe62df5615407187701ad00
Administrator <Administrator@ptj-PC>
2016-12-05 19:59:47 +0900

    연습소스업로드

---
+++ Algorithm/src/Array2testpractice.java
@@ -0,0 +1,21 @@
+import java.io.FileInputStream;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Array2testpractice{
+
+	static int test[][] = {{4,3},{2,6},{7,2},{1,4}};
+	static int ans;
+
+	public static void main(String args[]) throws Exception {
+
+		Scanner sc = new Scanner(System.in);
+
+
+
+		System.out.println(Arrays.deepToString(test));
+
+	}
+}
